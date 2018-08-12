112520360770a82207ecc8acc8ab4c581ea81475
jcaraballo <jcaraballo@users.noreply.github.com>
2018-08-12 15:59:43 +0100

    Fix yarn installation

--- boot/520-install-yarn.bash
+++ boot/520-install-yarn.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
-&& echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \
-apt-get update && sudo apt-get install yarn
+&& echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list \
+&& apt-get update && apt-get install yarn
 
