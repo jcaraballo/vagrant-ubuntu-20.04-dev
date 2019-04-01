e35c0ebd0a5a3dec16e44149754077fa26fca055
jcaraballo <jcaraballo@users.noreply.github.com>
2019-04-01 11:11:09 +0100

    Upgrade node to 10

--- boot/510-install-node-all-users.bash
+++ boot/510-install-node-all-users.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - \
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - \
 && sudo apt-get install -y nodejs
 
