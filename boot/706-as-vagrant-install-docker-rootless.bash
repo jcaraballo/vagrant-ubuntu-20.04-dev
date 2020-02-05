9cf1cc2eab4c0b7e318a39f990e84f540e763810
jcaraballo <jcaraballo@users.noreply.github.com>
2020-02-05 13:54:24 +0000

    docker-rootless installation: Use $HOME
    (instead of hardcoding it in the script)

--- boot/706-as-vagrant-install-docker-rootless.bash
+++ boot/706-as-vagrant-install-docker-rootless.bash
@@ -2,6 +2,6 @@
 
 curl -sSL https://get.docker.com/rootless | sh
 
-echo 'export PATH=/home/vagrant/bin:$PATH' >> $HOME/.bashrc
+echo "export PATH=$HOME/bin:"'$PATH' >> $HOME/.bashrc
 echo 'export DOCKER_HOST=unix:///run/user/1000/docker.sock' >> $HOME/.bashrc
 
