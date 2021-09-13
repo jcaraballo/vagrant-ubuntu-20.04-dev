64d8189321649ddc68188be089c02a3620322637
jcaraballo <jcaraballo@users.noreply.github.com>
2021-09-13 18:14:07 +0100

    ssh keys copying: Handle Ed25519 keys

--- boot/101-maybe-set-up-ssh-keys.bash
+++ boot/101-maybe-set-up-ssh-keys.bash
@@ -3,8 +3,11 @@ keys=$1
 
 if [ -d "${keys}" ]; then
   if [ -f "${keys}/id_rsa" ]; then
-    echo 'Adding ssh keys'
+    echo 'Adding RSA ssh keys'
     mkdir -p ${HOME}/.ssh/ && cp ${keys}/id_rsa.pub ${keys}/id_rsa ${HOME}/.ssh/ 
+  elif [ -f "${keys}/id_ed25519" ]; then
+    echo 'Adding Ed25519 ssh keys'
+    mkdir -p ${HOME}/.ssh/ && cp ${keys}/id_ed25519.pub ${keys}/id_ed25519 ${HOME}/.ssh/ 
   else
     echo "${keys} directory doesn't contain private key id_rsa, skipping ssh keys"
   fi
