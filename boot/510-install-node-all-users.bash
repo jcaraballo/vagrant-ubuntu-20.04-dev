ae26b56e158a59ec89446ffd484b30ddf8c161dd
jcaraballo <jcaraballo@users.noreply.github.com>
2022-04-01 18:52:04 +0100

    Bump node to 16

--- boot/510-install-node-all-users.bash
+++ boot/510-install-node-all-users.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - \
+curl -sSL https://deb.nodesource.com/setup_16.x | sudo -E bash - \
 && sudo apt-get install -y nodejs
 
